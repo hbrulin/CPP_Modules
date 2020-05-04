3a0b311abcffb0e8ae6703f277001f74a94eb567
babaoriley <helenebrulin@live.fr>
2020-05-04 18:35:49 +0200

    06x01

--- rendus/Module_06/ex01/main.cpp
+++ rendus/Module_06/ex01/main.cpp
@@ -1,15 +1,22 @@
 #include "serial.hpp"
 
+/* Reinterpret cast : instructs the compiler to treat expression as if it had the type new_type.
+Dans cet exercice, on ne peut pas utiliser static_cast car un int * est un pointeur sur 4-byte, et un char *
+un pointeur sur un seul. Static_cast empêcherait ce type de cast à la compilation.
+Or on veut écrire un int sur 4 byte entre deux suites de char d'un byte qu'on write un par un. */
+
 void * serialize(void)
 {
+	srand(time(NULL));
 	static std::string alnum = "0123456789" \
 		"abcdefghijklmopqrstuvxyz" \
 		"ABCDEFGHIJKLMOPQRSTUVXYZ";
 	static size_t size = alnum.size();
-	uint8_t *raw = new uint8_t[8 + 4 + 8]; //uint8_t is the same as a byte.
-    for (size_t i = 0; i < 8; i++) //size_t can store the maximum size of a theoretically possible object of any type. good for array indexing
+	/* uint8_t is the same as a byte. */
+	uint8_t *raw = new uint8_t[8 + 4 + 8]; 
+    for (size_t i = 0; i < 8; i++)
         raw[i] = alnum[rand() % size];
-    *reinterpret_cast<int*>(raw + 8) = rand() % 42; //j'utilise ça car je ne peux pas cast en static sinon ça change tout bytes
+    *reinterpret_cast<int*>(raw + 8) = rand() % 42;
     for (size_t i = 0; i < 8; i++)
         raw[8 + 4 + i] = alnum[rand() % size];
     return reinterpret_cast<void*>(raw);
@@ -20,7 +27,7 @@ Data * deserialize(void * raw)
 	Data *data = new Data;
 
 	data->s1 = std::string(static_cast<char*>(raw), 8);
-	data->n = *reinterpret_cast<int*>(static_cast<char*>(raw) + 8); //car là je prends 4 bytes à la fois
+	data->n = *reinterpret_cast<int*>(static_cast<char*>(raw) + 8);
 	data->s2 = std::string(static_cast<char*>(raw) + 12, 8);
 	return (data);
 }
